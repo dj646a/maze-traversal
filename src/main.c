84f490caaa8bc060db106add0a04ccf13dc45268
dj <117040210+dj646a@users.noreply.github.com>
2022-11-06 23:33:10 +0000

    Project TODOs

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+    TODO:
+
+    Create a window.
+    Create a pixel buffer.
+    Blit to a pixel buffer.
+
+    Create a maze struct.
+    Blit a maze struct.
+
+    Generate VALID mazes, entrances on the outtermost left and exits on the outtermost right.
+    Visualize the path taken to validate the maze.
+
+    For every generated maze, perform, visualize and benchmark:
+        1. Depth first search.
+        2. Dijkstra's algorithm
+        3. A* search algorithm
+*/
+
 int main(void)
 {
     printf("Maze traversal\n");
